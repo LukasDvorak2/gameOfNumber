842df70816a985a32ca1cef724894f82295cf3e1
LukasDvorak2 <it-lukasdvorak@stredniskola.net>
2024-06-05 11:02:41 +0200

    Úvodní nastavení aplikace, inicializace proměnných a připravení generatoru

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "color.h" 
+ /*Hadani tajneho cisla*/
+using namespace std;
+
+int main(int argc, char *argv[]){
+    system("chcp 65001>01");
+ int tajneCislo;
+ int hracovoCislo;
+ tajneCislo = rand ()%5+1; //nahodne cislo do 1 do 5
+ cout << "Zadej svuj typ na tajne cislo: ";
+ cin >> hracovoCislo;
+return 0;
+}
